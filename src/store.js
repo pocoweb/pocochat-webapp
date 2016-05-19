58d4955c61cc4627206925b086bfd935827fbe3f
zhuyiting <zhuyiting@hyron.com>
2016-05-19 13:31:20 +0800

    skip current user when load users

--- src/store.js
+++ src/store.js
@@ -3,8 +3,8 @@ const KEY_CHAT = STORAGE_KEY + '-CHAT';
 const KEY_TIME = STORAGE_KEY + '-TIME';
 
 Parse.initialize("pocoweb-chat", "njLwbUJgejKCjC2y");
-Parse.serverURL = 'http://pocoweb.com:11337/parse';
-//Parse.serverURL = 'http://localhost:1337/parse';
+//Parse.serverURL = 'http://pocoweb.com:11337/parse';
+Parse.serverURL = 'http://localhost:1337/parse';
 
 var ParseSession = Parse.Object.extend('Messages');
 
@@ -23,12 +23,12 @@ function loadUserByID(uid, callback) {
 
 export default {
     getAIId() {
-        return 'PhU3ki2QQV'; //release
-        //return '1E5T8KLWbF'; //debug
+        //return 'PhU3ki2QQV'; //release
+        return '1E5T8KLWbF'; //debug
     },
     getGroupId() {
-        return '7SDMDWUPFh'; //release
-        //return 'qjm7IQwids'; //debug
+        //return '7SDMDWUPFh'; //release
+        return 'qjm7IQwids'; //debug
     },
     loadSession(user, callback) {
         console.log('>>> loadSession')
@@ -73,7 +73,7 @@ export default {
                 
                 var query = new Parse.Query('User');
                 query.equalTo('group', currentUser.get('group'));
-                query.notEqualTo('ObjectId', currentUser.id);
+                query.notEqualTo('objectId', currentUser.id);
                 return query.find();
                 
             }).then(function(users) {
